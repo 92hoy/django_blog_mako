5df0eec7a5375e9c6e873acb959d12962aaaa9de
92hoy <ho_9209@naver.com>
2018-07-27 18:15:55 +0900

    login page layor

--- backend/urls.py
+++ backend/urls.py
@@ -4,11 +4,13 @@ from django.conf.urls import url
 
 #login / logout
 from .djangoapps.login import views as LoginViews
+from .djangoapps.board import views as BoardViews
 from .djangoapps.logout import views as LogoutViews
 
 from .djangoapps.main import views as MainViews
 
 urlpatterns = [
     url('login$', LoginViews.login, name='login'),
+    url('board$', BoardViews.board, name='board'),
     url('$', MainViews.main, name='main'),
 ]
