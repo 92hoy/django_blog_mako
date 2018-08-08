0bb9211a7ef59d89de4483a49a147fdf03e53100
92hoy <ho_9209@naver.com>
2018-08-08 16:23:46 +0900

    django orm board insert fail

--- backend/djangoapps/board/views.py
+++ backend/djangoapps/board/views.py
@@ -14,19 +14,30 @@ from logging import handlers
 from backend.models import User
 from backend.models import Board
 
+
 def board(request):
 
     logging.debug('게시판 페이지 접속')
 
+    b_data = Board.objects.all()
+    print("data all = ",b_data)
     return render(request, 'board/board.html')
 
 def board_add(request):
 
+
     if request.is_ajax():
+
         title = request.POST.get('title_input')
         content = request.POST.get('content_input')
+        me = User.objects.get(username='JHY')
+
+        board_add = Board(user_id=me, title=title, content=content)
+        board_add.save()
+
         print(title)
         print(content)
+        logging.debug('게시글 작성')
 
 
     JsonResponse({'return':'success'})
